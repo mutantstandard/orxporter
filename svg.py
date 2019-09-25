4548b98392beeb2a89915aac29f269c10293b3b6
Dzuk <32108484+dzuk-mutant@users.noreply.github.com>
2019-09-25 07:24:53 +0100

    Added missing svg changes from a previous commit [Important bug fix!]
    For some reason the changes to the svg.py mentioned in a previous commit were ticked out or not saved??? IDK. It's here now.

--- svg.py
+++ svg.py
@@ -1,6 +1,11 @@
 import re
 
-def ctrans(svg, pfrom, pto):
+def translate_color(svg, pfrom, pto):
+    """
+    Translates colours from a source file's original colours into new colours.
+    """
+
+    # looks for incidences of one colour, compares it and replaces it.
     res = svg
     for centry, ccol in pfrom.items():
         cr = ccol + ';'
@@ -15,12 +20,18 @@ def ctrans(svg, pfrom, pto):
     return res
 
 def add_license(svg, license_data):
+    """
+    Adds licensing metadata to an SVG.
+    """
     svgidx = svg.index('<svg')
     insidx = svg.index('>', svgidx) + 1
     metastr = '\n<metadata>\n' + license_data + '</metadata>\n'
     return svg[:insidx] + metastr + svg[insidx:]
 
 def get_viewbox_size(svg):
+    """
+    Returns the size of an SVG's viewbox (as a tuple containing width and height).
+    """
     try:
         x1, y1, x2, y2 = list(map(
             int, svg[svg.index('viewBox'):].split('"', 2)[1].split()))
